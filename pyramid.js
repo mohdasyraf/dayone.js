329b16ebf8e8eb844cbbc8c198427f710163267d
mohdasyraf <77966272+mohdasyraf@users.noreply.github.com>
2021-02-02 20:34:16 +0800

    Create pyramid.js

---
+++ pyramid.js
@@ -0,0 +1,14 @@
+// Exercise
+var lines = 6
+for(var i = 0; i < lines; i++){
+  for(var j = 0; j < i+1; j ++){
+  document.write("#");
+}
+  document.write("<br/>")
+}
+for(var i = 0; i < lines; i++){
+  for(var j = 0; j < lines - i-1; j ++){
+  document.write("#");
+}
+  document.write("<br/>")
+}
